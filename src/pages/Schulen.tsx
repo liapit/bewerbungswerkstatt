679ddaed90677b1a473fa6f4f56508480070f040
Daniel Babbev <dannybabbev@gmail.com>
2026-04-01 11:45:18 +0200

    move old page

---
+++ src/pages/Schulen.tsx
@@ -0,0 +1,25 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Problem from "@/components/Problem";
+import Benefits from "@/components/Benefits";
+import Offers from "@/components/Offers";
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+
+const Schulen = () => {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Hero />
+      <Problem />
+      <Benefits />
+      <Offers />
+      <About />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default Schulen;
